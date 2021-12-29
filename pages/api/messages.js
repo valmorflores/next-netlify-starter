244f52cbfa3e42ae9fd65da9db14b2998a0acd71
Valmor Pereira Flores <valmorflores@gmail.com>
2021-12-29 14:55:42 -0300

    update toendpoint /api/messages

--- pages/api/messages.js
+++ pages/api/messages.js
@@ -1,7 +1,8 @@
 export default async (req, res) => {
     const result = await fetch('https://criativa.app/work/servers/sei1/gestorequipes/api/v1/version/0');
     const resultJson = await result.json();
-    res.status(200).json(
-        resultJson
+    res.status(200).json( {
+            changelog : resultJson.system.change_log
+        }
     );
 }
