d732ac722d6fc0a87fc4554dd5225ba1bcba35d0
Valmor Pereira Flores <valmorflores@gmail.com>
2021-12-29 14:59:22 -0300

    Api internal changelog endpoint

---
+++ pages/api/changelog.js
@@ -0,0 +1,8 @@
+export default async (req, res) => {
+    const result = await fetch('https://criativa.app/work/servers/sei1/gestorequipes/api/v1/version/0');
+    const resultJson = await result.json();
+    res.status(200).json( {
+            data : resultJson.system.change_log
+        }
+    );
+}
