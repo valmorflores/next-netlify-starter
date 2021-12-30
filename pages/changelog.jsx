e60528e56a586690af8f278b24d1788a659fc739
Valmor Pereira Flores <valmorflores@gmail.com>
2021-12-30 08:12:47 -0300

    update for changelog page show

---
+++ pages/changelog.jsx
@@ -0,0 +1,38 @@
+
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Changelog({name}) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Information from API" />
+        <p className="description">
+            Getting remote information 
+        </p>
+        <p className="description">
+           <span><strong>{name}</strong></span>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('https://criativa.app/work/servers/sei1/gestorequipes/api/v1/version/0');
+    const json = await res.json();
+    return {
+      props: {
+        name: json.system.software    
+      }
+    }
+  }
+  
